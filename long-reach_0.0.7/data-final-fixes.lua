47fe6c4c12491986931627bf8bc495aa5efec227
Matthew Walker <utoxin@gmail.com>
2017-04-26 08:20:39 -0600

    Changing default build distances

--- long-reach_0.0.7/data-final-fixes.lua
+++ long-reach_0.0.7/data-final-fixes.lua
@@ -1,2 +1,5 @@
-data.raw.player.player.build_distance = 10000
-data.raw.player.player.reach_distance = 10000
+-- Rational: Pulling items out of machines is a nice timesaver, building 
+-- lets you do things like spam turrets into biter nests, so I think 
+-- building should be shorter range than adding / removing items
+data.raw.player.player.build_distance = 150
+data.raw.player.player.reach_distance = 300
