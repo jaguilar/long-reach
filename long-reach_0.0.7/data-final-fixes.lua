67de4a45776bccc544cc1c948a7be1cb70754d44
Matthew Walker <utoxin@gmail.com>
2017-04-26 19:12:47 -0600

    Removing extra comments, and unifying values
    This is the simple change to make this more reasonable. A larger change that uses the config system is being worked on.

--- long-reach_0.0.7/data-final-fixes.lua
+++ long-reach_0.0.7/data-final-fixes.lua
@@ -1,5 +1,2 @@
--- Rational: Pulling items out of machines is a nice timesaver, building 
--- lets you do things like spam turrets into biter nests, so I think 
--- building should be shorter range than adding / removing items
-data.raw.player.player.build_distance = 150
-data.raw.player.player.reach_distance = 300
+data.raw.player.player.build_distance = 125
+data.raw.player.player.reach_distance = 125
